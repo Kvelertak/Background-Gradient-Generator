59a4de97838c2ad0efb10fbf96f733af195db023
Tyler W <57962091+Kvelertak@users.noreply.github.com>
2023-05-22 19:00:56 -0400

    Add files via upload
    Added randomize button which selects two random gradients as the selections, instead of a user-selected gradient.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.querySelector("button");
 
 
 function setGradient() {
@@ -14,6 +15,29 @@ function setGradient() {
     css.textContent = body.style.background + ";";
 }
 
+function randomGradient(){
+    var rand1 = colorRandomize();
+    var rand2 = colorRandomize();
+    
+    body.style.background = "linear-gradient(to right, "
+    + rand1
+    + ", "
+    + rand2
+    + ")";
+
+    css.textContent = body.style.background + ";";
+}
+
+function colorRandomize(){
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
+
+
+button.addEventListener("click", randomGradient);
 
 color1.addEventListener("input", setGradient);
 
